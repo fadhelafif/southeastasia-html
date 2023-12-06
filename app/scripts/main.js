d7c9d6eb5f7c1d0e065019f62cd551003aef4042
Tofan <tofan.rukmana@bankina.id>
2023-12-06 12:50:01 +0700

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,21 +4,21 @@ $(document).ready(function() {
     var scroll = $(window).scrollTop();
 
     if (scroll > 50) { // Ubah angka 100 dengan tinggi scroll ketika efek ingin dimulai
-      $(".navbar").addClass("navbar-scrolled");
+      $('.navbar').addClass('navbar-scrolled');
     } else {
-      $(".navbar").removeClass("navbar-scrolled");
+      $('.navbar').removeClass('navbar-scrolled');
     }
   });
 
-  $(".dropdown-toggle").click(function(e) {
+  $('.dropdown-toggle').click(function(e) {
     e.preventDefault(); // Mencegah tautan default
 
-    $(this).toggleClass("show");
-    $(this).next('.dropdown-menu').toggleClass("show"); // Menampilkan dropdown-menu yang terkait dengan dropdown-toggle yang diklik
+    $(this).toggleClass('show');
+    $(this).next('.dropdown-menu').toggleClass('show'); // Menampilkan dropdown-menu yang terkait dengan dropdown-toggle yang diklik
 
     // Menyembunyikan dropdown-menu lain jika ada yang terbuka
-    $(".dropdown-menu").not($(this).next('.dropdown-menu')).removeClass("show");
-    $(".dropdown-toggle").not($(this)).removeClass("show");
+    $('.dropdown-menu').not($(this).next('.dropdown-menu')).removeClass('show');
+    $('.dropdown-toggle').not($(this)).removeClass('show');
   });
   
 });
